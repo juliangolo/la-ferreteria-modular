ef534818f97931fdbc116a5d1d11c7e889644ea3
juliangolo <juliangolo@yahoo.es>
2026-04-17 21:45:44 +0200

    Día 3 completado: Máquina registradora con if/else y actualización de stock

--- app.mjs
+++ app.mjs
@@ -2,22 +2,42 @@
 import { inventario } from "./productos.mjs";
 
 // 2. FUNCIÓN: Creamos la máquina. Entra un nombre, sale un número (dinero).
-function calcularDineroEnStock(nombreHerramienta) {
+function venderYActualizar(nombreHerramienta, cantidadPedida) {
+
+    console.log('---------------------------------------------');
+    console.log('Intentando vender ' + cantidadPedida + ' ' + nombreHerramienta + '...');
 
     // Paso A: Buscamos en la lista la caja que coincida con el nombre que entra en la máquina
-    const productoEncontrado = inventario.find(item => item.nombre === nombreHerramienta);
+    const caja = inventario.find(item => item.nombre === nombreHerramienta);
 
     // Paso B: Calculamos el dinero multiplicando el precio por el stock de la caja que hemos encontrado
-    const dineroTotal = productoEncontrado.precio * productoEncontrado.stock;
+    if (cantidadPedida > caja.stock) {
 
-    // Paso C: La máquina escupe ( devuelve) el resultado
-    return dineroTotal;
+        // Si pide más de lo que hay, la máquina escupe un error
+        return '❌ Error: No se puede vender. Solo nos quedan ' + caja.stock + ' unidades.';
+
+    } else {
+
+        // Si hay stock suficiente, calculamos el dinero a cobrar.
+        // MULTIPLICA el precio de la caja por lo que ha pedido el cliente.
+        const total = caja.precio * cantidadPedida;
+
+        caja.stock = caja.stock - cantidadPedida;
+
+
+
+        return '✅ Cobrados ' + total + '€. El nuevo stock de ' + nombreHerramienta + ' es: ' + caja.stock;
+    
+    }
 }
 
-// 3. USO DE LA MÁQUINA: Queremos saber cuánto dinero tenemos en Taladros
-console.log('Calculando el valor del invetario de Taladro...');
 
-// Llamamos a la máquina y le metemos la palabra "Taladro"
-const valorTaladros = calcularDineroEnStock('Taladro');
+// 3. PRUEBAS DE FUEGO 
+// 1. Vendemos 1 Taladro ( Habia 2. Deberia quedar 1)
+console.log( venderYActualizar('Taladro', 1) );
 
-console.log('Tenemos un total de ' + valorTaladros + ' euros en Taladros.');
+// 2, Viene otro cliente y se lleva otro Taladro (Quedan 1. Deberían quedar 0)
+console.log( venderYActualizar('Taladro', 1) );
+
+// 3. Viene un tercer cliente pidiendo 1 Taladro ( ¡Debería saltar el ERROR del if!)
+console.log( venderYActualizar('Taladro', 1) );
