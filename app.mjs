3521eff14fc519160fee189c66174ac984c0a4eb
juliangolo <juliangolo@yahoo.es>
2026-04-20 11:48:45 +0200

    Reforzando Objetos, Array, Functions

--- app.mjs
+++ app.mjs
@@ -1,43 +1,34 @@
-// 1. IMPORTACIÓN: Trae la lista de cajas desde el otro archivo
-import { inventario } from "./productos.mjs";
+import { inventario } from './productos.mjs';
 
-// 2. FUNCIÓN: Creamos la máquina. Entra un nombre, sale un número (dinero).
-function venderYActualizar(nombreHerramienta, cantidadPedida) {
+function procesarDevolucion(nombreHerraamienta, cantidadDevuelta, estaRota) {
 
-    console.log('---------------------------------------------');
-    console.log('Intentando vender ' + cantidadPedida + ' ' + nombreHerramienta + '...');
+    console.log('\n--- Cliente devolviendo ' + cantidadDevuelta + ' ' + nombreHerraamienta + '---');
 
-    // Paso A: Buscamos en la lista la caja que coincida con el nombre que entra en la máquina
-    const caja = inventario.find(item => item.nombre === nombreHerramienta);
+    // 1. Buscamos la caja en el almacén 
+    const caja = inventario.find(item => item.nombre === nombreHerraamienta);
 
-    // Paso B: Calculamos el dinero multiplicando el precio por el stock de la caja que hemos encontrado
-    if (cantidadPedida > caja.stock) {
+    // 2. Tomamos la decisión: ¿Está rota?
+    if (estaRota === true) {
 
-        // Si pide más de lo que hay, la máquina escupe un error
-        return '❌ Error: No se puede vender. Solo nos quedan ' + caja.stock + ' unidades.';
+        // SI ESTÁ ROTA, no la volvemos a poner en el estante (va a la basura).
+        // Solo calculamos cuánto dinero hay que devolver al cliente.
+        const dineroADevolver = caja.precio * cantidadDevuelta;
+        return '⚠️ Defectuoso. Directo a la basura. Devolver al cliente: ' + dineroADevolver + '€';
 
     } else {
 
-        // Si hay stock suficiente, calculamos el dinero a cobrar.
-        // MULTIPLICA el precio de la caja por lo que ha pedido el cliente.
-        const total = caja.precio * cantidadPedida;
+        // Si NO está rota, la ponemos en el estante ( SUMAMOS al stock ) y devolvemos el dinero.
+        caja.stock = caja.stock + cantidadDevuelta;
 
-        caja.stock = caja.stock - cantidadPedida;
+        const dineroADevolver = caja.precio * cantidadDevuelta
+        return '✅ Aceptada. Nuevo stock de ' + nombreHerraamienta + ' es: ' + caja.stock + '. Devolver: ' + dineroADevolver + '€';
 
-
-
-        return '✅ Cobrados ' + total + '€. El nuevo stock de ' + nombreHerramienta + ' es: ' + caja.stock;
-    
     }
 }
 
+// --- PRUEBAS DE FUEGO ---
+// 1, Devuelven 1 Taladro roto (El stock debería quedarse igual: 2)
+console.log(procesarDevolucion('Taladro', 1, true) );
 
-// 3. PRUEBAS DE FUEGO 
-// 1. Vendemos 1 Taladro ( Habia 2. Deberia quedar 1)
-console.log( venderYActualizar('Taladro', 1) );
-
-// 2, Viene otro cliente y se lleva otro Taladro (Quedan 1. Deberían quedar 0)
-console.log( venderYActualizar('Taladro', 1) );
-
-// 3. Viene un tercer cliente pidiendo 1 Taladro ( ¡Debería saltar el ERROR del if!)
-console.log( venderYActualizar('Taladro', 1) );
+// 2. Devuelven 2 Martillos en buen estado (El stock debería subir a 6)
+console.log(procesarDevolucion('Martillo', 2, false) );
