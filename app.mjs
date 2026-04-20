3abef821afc0942e84fdcd271a334174496b584d
juliangolo <juliangolo@yahoo.es>
2026-04-20 12:46:04 +0200

    Asicronia añadiendo async, await, try y catch

--- app.mjs
+++ app.mjs
@@ -1,9 +1,36 @@
-console.log('PASO 1: El cliente 1 entra y pide una Sierra pesada del fondo del almacén.');
+// 1. EL MOZO LENTO( No toques esta función, es el mozo trabajando)
+// Fíjate que devuelve una "Promise" (El Busca del restaurante)
+function irAlFondoDelAlmacen(herramienta) {
+    return new Promise((resolver, rechazar) => {
+        setTimeout(() => {
+            if (herramienta === 'Destornillador Mágico') {
+                // Si le pedimos esto, el mozo falla y RECHAZA la promesa
+                rechazar('❌ ¡Jefe, he buscado por todas partes pero no existe!');
+            } else {
+                resolver(herramienta);
+            }
+        }, 2000);
+    });
+}
 
-// Llamamos a la función asíncrona (el mozo lento)
-setTimeout(() => {
-    console.log('PASO 2: El mozo por fin llega con la Sierra del cliente 1.');
-}, 3000); 
- 
-console.log('PASO 3: Atendemos al cliente 2 que solo quería un paquete de chicles rápido.');
+// 2. EL ENCARGADO DEL MOSTRADOR
+// Como el encargado va a tener que ESPERAR, la función tiene que ser asincrona.
+// HUECO 1: Pon la palabra mágica para avisar de que esta función usa tiempos de espera
+async function atenderMostrador() {
 
+    console.log('PASO 1: Cliente VIP pide un Destornillador Mágico...');
+    try {
+        // Intentamos esperar al mozo...
+        const cajaRecibida = await irAlFondoDelAlmacen('Destornillador Mágico');
+        console.log('PASO 2: El mozo trajo la ' + cajaRecibida);
+    
+    }
+    catch (error) {
+
+        console.log('PASO 2: (PROBLEMA): El mozo dice -> ' + error);
+        console.log('PASO 3: Pedimos disculpas al cliente y le ofrecemos otra cosa.');
+    }
+
+}
+
+atenderMostrador();
