b86d2edba3d2536541f8b07fde0faa1ae39f8c67
juliangolo <juliangolo@yahoo.es>
2026-04-20 12:00:10 +0200

    Empezando con la asíncronia

--- app.mjs
+++ app.mjs
@@ -1,34 +1,9 @@
-import { inventario } from './productos.mjs';
+console.log('PASO 1: El cliente 1 entra y pide una Sierra pesada del fondo del almacén.');
 
-function procesarDevolucion(nombreHerraamienta, cantidadDevuelta, estaRota) {
+// Llamamos a la función asíncrona (el mozo lento)
+setTimeout(() => {
+    console.log('PASO 2: El mozo por fin llega con la Sierra del cliente 1.');
+}, 3000); 
+ 
+console.log('PASO 3: Atendemos al cliente 2 que solo quería un paquete de chicles rápido.');
 
-    console.log('\n--- Cliente devolviendo ' + cantidadDevuelta + ' ' + nombreHerraamienta + '---');
-
-    // 1. Buscamos la caja en el almacén 
-    const caja = inventario.find(item => item.nombre === nombreHerraamienta);
-
-    // 2. Tomamos la decisión: ¿Está rota?
-    if (estaRota === true) {
-
-        // SI ESTÁ ROTA, no la volvemos a poner en el estante (va a la basura).
-        // Solo calculamos cuánto dinero hay que devolver al cliente.
-        const dineroADevolver = caja.precio * cantidadDevuelta;
-        return '⚠️ Defectuoso. Directo a la basura. Devolver al cliente: ' + dineroADevolver + '€';
-
-    } else {
-
-        // Si NO está rota, la ponemos en el estante ( SUMAMOS al stock ) y devolvemos el dinero.
-        caja.stock = caja.stock + cantidadDevuelta;
-
-        const dineroADevolver = caja.precio * cantidadDevuelta
-        return '✅ Aceptada. Nuevo stock de ' + nombreHerraamienta + ' es: ' + caja.stock + '. Devolver: ' + dineroADevolver + '€';
-
-    }
-}
-
-// --- PRUEBAS DE FUEGO ---
-// 1, Devuelven 1 Taladro roto (El stock debería quedarse igual: 2)
-console.log(procesarDevolucion('Taladro', 1, true) );
-
-// 2. Devuelven 2 Martillos en buen estado (El stock debería subir a 6)
-console.log(procesarDevolucion('Martillo', 2, false) );
